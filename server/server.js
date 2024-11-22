a0efc5b74c84486d9aac340b72baa293eadc3d02
SANJAI-SANSPARK <sanjaisenthil2k@gmail.com>
2024-11-22 21:54:28 +0530

    sucessfully working all crud operations

--- server/server.js
+++ server/server.js
@@ -36,6 +36,53 @@ app.post("/add_user", (req, res) => {
     });
 });
 
+app.get("/students", (req, res) => {
+    const sql = "SELECT * FROM student_details";
+    db.query(sql, (err, result) => {
+      if (err) res.json({ message: "Server error" });
+      return res.json(result);
+    });
+  });
+
+app.get("/get_student/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM student_details WHERE `id`= ?";
+    db.query(sql, [id], (err, result) => {
+      if (err) res.json({ message: "Server error" });
+      return res.json(result);
+    });
+  });
+
+
+  app.post("/edit_user/:id", (req, res) => {
+    const id = req.params.id;
+    const sql =
+      "UPDATE student_details SET `name`=?, `email`=?, `age`=?, `gender`=? WHERE id=?";
+    const values = [
+      req.body.name,
+      req.body.email,
+      req.body.age,
+      req.body.gender,
+      id,
+    ];
+    db.query(sql, values, (err, result) => {
+      if (err)
+        return res.json({ message: "Something unexpected has occured" + err });
+      return res.json({ success: "Student updated successfully" });
+    });
+  });
+
+  app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "DELETE FROM student_details WHERE id=?";
+    const values = [id];
+    db.query(sql, values, (err, result) => {
+      if (err)
+        return res.json({ message: "Something unexpected has occured" + err });
+      return res.json({ success: "Student updated successfully" });
+    });
+  });
+
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
